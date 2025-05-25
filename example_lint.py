3dc9fcd2dfe385ea2df15838e3d5b3d76807a2d2
Polina Voropay <voropaypa@gmail.com>
2025-05-25 18:32:04 +0300

    Added first func on masks, added linters (prev HW)

---
+++ example_lint.py
@@ -0,0 +1,17 @@
+# from typing import Union
+
+# def say_hello():
+#     print("Hello, World")
+
+
+# def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+#     """Функция, которая складывает два числа"""
+#     return a + b
+#
+#
+# print(add(5, 5))
+
+# print(help(add))
+
+price: int = 5
+title: str
---
+++ src/masks.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[int]) -> Union[str]:
+    """Принимает на вход номер карты и возвращает ее маску в формате XXXX XX** **** XXXX,
+    где X — это цифра номера."""
+    card_number_str = str(card_number)
+    return f"{card_number_str[0:4]} {card_number_str[4:6]}** **** {card_number_str[12:]}"
+
+
+def get_mask_account(account_number: Union[int]) -> Union[str]:
+    """Принимает на вход номер счета и возвращает его маску.
+    Номер счета замаскирован и отображается в формате **XXXX, где X — это цифра номера."""
+    account_number_str = str(account_number)
+    return f"**{account_number_str[-4:]}"
+
+
+print(get_mask_card_number(7000792289606361))
+print(get_mask_account(73654108430135874305))
