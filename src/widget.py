0d344927a4817f4853c172de0ecef9ab42269bfa
Polina Voropay <voropaypa@gmail.com>
2025-05-25 19:35:19 +0300

    Added func fot getting mask account card

---
+++ src/widget.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+
+
+def mask_account_card(card_info: Union[str]) -> Union[str]:
+    card_alpha = ""
+    card_digit = ""
+    for symbol in card_info:
+        if symbol.isalpha():
+            card_alpha += symbol
+        if symbol == " ":
+            card_alpha += symbol
+        elif symbol.isdigit():
+            card_digit += symbol
+    if len(card_digit) == 16:
+        card_mask = get_mask_card_number(card_digit)
+    elif len(card_digit) == 20:
+        card_mask = get_mask_account(card_digit)
+    return card_alpha + card_mask
+
+print(mask_account_card("Счет 73654108430135874305"))
