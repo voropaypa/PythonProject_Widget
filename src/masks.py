78501a1a270f13444e6cf7d66f323b3ee99d36e3
Polina Voropay <voropaypa@gmail.com>
2025-05-25 19:57:24 +0300

    hastagged examples in previous masks

--- src/masks.py
+++ src/masks.py
@@ -15,5 +15,5 @@ def get_mask_account(account_number: Union[int]) -> Union[str]:
     return f"**{account_number_str[-4:]}"
 
 
-print(get_mask_card_number(7000792289606361))
-print(get_mask_account(73654108430135874305))
+# print(get_mask_card_number(7000792289606361))
+# print(get_mask_account(73654108430135874305))
