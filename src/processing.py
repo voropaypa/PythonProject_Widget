2c4d5839c0df85c5bfdd3cfaee4180bd4859a0eb
Polina Voropay <voropaypa@gmail.com>
2025-06-03 18:33:11 +0300

    Added sort_by_date func, cleared module from testing

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,10 @@
 from typing import Union
 
+
 def filter_by_state(list_of_dictionaries: Union[list], key) -> Union[list]:
-    """Функция получает на вход список словарей и ключ, и возвращает только те словари,
-    у которых статус(state) соответствует заданному функцией условию(key)"""
+    """Функция сортирует по статусу исполнения.
+    Функция получает на вход список словарей и ключ, и возвращает только те словари,
+    у которых статус(state) соответствует заданному функцией условию(key)."""
     new_dict_list = list()
     for dictionary in list_of_dictionaries:
         if dictionary['state'] == key:
@@ -10,12 +12,8 @@ def filter_by_state(list_of_dictionaries: Union[list], key) -> Union[list]:
     return new_dict_list
 
 
-# print(
-#     filter_by_state(
-#         [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-#          {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-#          {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-#          {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
-#         'CANCELED'
-#     )
-# )
+def sort_by_date(list_of_dicts: Union[list], reverse=True) -> Union[list]:
+    """Функция сортирует по дате исполнения.
+    Функция получает на вход список словарей и порядок сортировки (по умолчанию - по убыванию),
+    и возвращает словари в заданном порядке, сортируя их по дате(ключ 'date')"""
+    return sorted(list_of_dicts, key=lambda x: x['date'], reverse=reverse)
