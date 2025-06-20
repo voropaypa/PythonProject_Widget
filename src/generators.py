1e5475fd0803c3f2b0fb8b943be2f9add41640fd
Polina Voropay <voropaypa@gmail.com>
2025-06-20 22:37:53 +0300

    Added tests for filter_by_currency, transaction_descriptions and modified said funcs accordingly

--- src/generators.py
+++ src/generators.py
@@ -1,21 +1,53 @@
 from typing import Union
 
+from pycodestyle import continued_indentation
+
 
 def filter_by_currency(transactions: Union[list], currency: Union[str]) -> Union[list]:
-    filter_result = [x for x in transactions if x["operationAmount"]["currency"]["name"] == currency]
-    for transaction in filter_result:
-        yield transaction
+    """Функция-генератор, которая принимает на вход список словарей,
+    представляющих транзакции. Функция возвращает итератор,
+    который поочередно выдает транзакции, где валюта операции
+    соответствует заданной (например, USD)."""
+    counter = 0
+    filter_result = [x for x in transactions if x["operationAmount"]["currency"]["code"] == currency]
+    if transactions == []:
+        raise ValueError("Ошибка. Транзакции не обнаружены.")
+    elif len(filter_result) == 0:
+        raise ValueError("Ошибка. Транзакций с заданной валютой не найдено.")
+    else:
+        for transaction in filter_result:
+            yield transaction
+            counter += 1
+            if len(filter_result) == counter:
+                raise ValueError("Все транзакции по заданной валюте были выведены.")
 
 
 def transaction_descriptions(transactions: Union[list]) -> Union[str]:
+    """Функция-генератор, которая принимает на вход список словарей,
+    представляющих транзакции. Функция возвращает итератор, который поочерёдно
+    возвращает описание каждой операции."""
+    counter = 0
     descriptions_list = [x["description"] for x in transactions]
-    for description in descriptions_list:
-        yield description
+    if transactions == []:
+        raise ValueError("Ошибка. Транзакции не обнаружены.")
+    else:
+        for description in descriptions_list:
+            yield description
+            counter += 1
+            if len(descriptions_list) == counter:
+                raise ValueError("Описания всех транзакций были выведены.")
 
 
 def card_number_generator(num1: Union[int], num2: Union[int]) -> Union[str]:
-    for num in range(num1, num2 + 1):
-        num_line = f"{num:016d}"
-        line_with_spaces = " ".join(num_line[i:i+4] for i in range(0, len(num_line), 4))
-        yield line_with_spaces
-        num += 1
+    """Функция-генератор, которая выдает поочерёдно банковские номера в формате
+    "XXXX XXXX XXXX XXXX" в заданном диапазоне от 0000 0000 0000 0001
+    до 9999 9999 9999 9999. Генератор принимает начальное и конечное значения
+    для генерации диапазона номеров."""
+    if num1 > 0 or num2 < 9999999999999999:
+        for num in range(num1, num2 + 1):
+            num_line = f"{num:016d}"
+            line_with_spaces = " ".join(num_line[i:i+4] for i in range(0, len(num_line), 4))
+            yield line_with_spaces
+            num += 1
+    else:
+        raise ValueError("Ошибка. Неправильный формат номера")
---
+++ tests/test_generators.py
@@ -0,0 +1,174 @@
+import pytest
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+
+@pytest.fixture
+def transactions():
+    return [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188"
+        },
+        {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        },
+        {
+            "id": 895315941,
+            "state": "EXECUTED",
+            "date": "2018-08-19T04:27:37.904916",
+            "operationAmount": {
+                "amount": "56883.54",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод с карты на карту",
+            "from": "Visa Classic 6831982476737658",
+            "to": "Visa Platinum 8990922113665229"
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {
+                "amount": "67314.70",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657"
+        }
+    ]
+
+
+def test_filter_by_currency(transactions):
+    """Проверка работы функции-генератора при корректном вводе данных
+    и наличии транзакций в заданной валюте"""
+    generator_usd = filter_by_currency(transactions, "USD")
+    assert next(generator_usd) == {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        }
+    generator_rub = filter_by_currency(transactions, "RUB")
+    assert next(generator_rub) == {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        }
+
+
+def test_filter_by_currency_without_transactions(transactions):
+    """Проверка работы функции-генератора при отсутствии транзакций
+    или операций в заданной валюте"""
+    generator_eur = filter_by_currency(transactions, "EUR")
+    with pytest.raises(ValueError):
+        assert next(generator_eur)
+
+
+def test_filter_by_currency_end_func(transactions):
+    """Проверка работы функции-генератора при его вызове, когда
+    все операции уже были выведены"""
+    generator_rub = filter_by_currency(transactions, "RUB")
+    assert next(generator_rub)
+    assert next(generator_rub)
+    with pytest.raises(ValueError):
+        assert next(generator_rub)
+
+
+def test_filter_by_currency_empty():
+    """Проверка работы функции-генератора при обработке пустого списка"""
+    generator = filter_by_currency([], "EUR")
+    with pytest.raises(ValueError):
+        assert next(generator)
+
+
+def test_transaction_descriptions(transactions):
+    """Проверка работы функции-генератора при корректном вводе данных
+    и выводе соответствующих описаний"""
+    generator = transaction_descriptions(transactions)
+    assert next(generator) == "Перевод организации"
+    assert next(generator) == "Перевод со счета на счет"
+
+
+def test_transaction_descriptions_empty(transactions):
+    """Проверка работы функции-генератора при вводе пустого списка
+    транзакций"""
+    generator = transaction_descriptions([])
+    with pytest.raises(ValueError):
+        assert next(generator)
+
+
+def test_transaction_descriptions_end_func(transactions):
+    """Проверка работы функции-генератора при его вызове, когда
+    все операции уже были выведены"""
+    generator = transaction_descriptions(transactions)
+    assert next(generator) == "Перевод организации"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод со счета на счет"
+    assert next(generator) == "Перевод с карты на карту"
+    assert next(generator) == "Перевод организации"
+    with pytest.raises(ValueError):
+        assert next(generator)
